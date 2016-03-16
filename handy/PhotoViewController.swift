3621028eef67f215fbe969457321ec87b381d8f6
Dima Osadchy <dima.osadchy@gmail.com>
2016-03-16 20:49:10 +0200

    new logic

--- handy/PhotoViewController.swift
+++ handy/PhotoViewController.swift
@@ -7,8 +7,6 @@
 //
 
 import UIKit
-import GPUImage
-import Nuke
 
 class PhotoViewController: UIViewController, UIScrollViewDelegate {
     
@@ -62,135 +60,140 @@ class PhotoViewController: UIViewController, UIScrollViewDelegate {
         
         NSLog("Nexus4Hand.sizeOfParentView = \(Nexus4Hand.sizeOfParentView)")
         NSLog("Size of UIScreen = \(UIScreen.mainScreen().bounds.size) ")
-        //Background blur imageView with originalPhoto
-        createBlurBackground(sizeMainImageView)
-
-        //On Screen imageView with originalPhoto
-        //createOnScreenScrollView(Nexus4Hand)
         
-        //Phone screen on
-        createPhoneSkinImage(Nexus4Hand)
-        
-        //TODO! Make all generics as possible
+        let filterMainImageView = FilterImage(imgView: mainImageView, image: image, phoneHand: Nexus4Hand)
+        let imgVi: UIImageView = filterMainImageView.filterImageView
+         mainImageView.addSubview(imgVi)
+//        //Background blur imageView with originalPhoto
+//        createBlurBackground(sizeMainImageView)
+//
+//        //On Screen imageView with originalPhoto
+//        //createOnScreenScrollView(Nexus4Hand)
+//        
+//        //Phone screen on
+//        createPhoneSkinImage(Nexus4Hand)
+//        
+//        //TODO! Make all generics as possible
         
     }
     
-    //MARK! - Imaage createion
-    func createPhoneSkinImage(phoneInfo: PhoneSkin) {
-        let phone = UIImageView()
-        
-        NSLog("mainImageView.bounds.size = \(mainImageView.bounds.size)")
-        NSLog("mainImageView.frame.size = \(mainImageView.frame.size)")
-
-        
-        phone.image = phoneInfo.handImage
-        phone.contentMode = .ScaleAspectFill
-        phone.userInteractionEnabled = true
-        phone.frame = CGRectMake(
-            0,
-            0,
-            UIScreen.mainScreen().bounds.size.width,  // should not be here
-            UIScreen.mainScreen().bounds.size.width   // should not be here
-        )
-        phone.addSubview(createOnScreenScrollView(phone, phoneInfo: phoneInfo))
-        mainImageView.addSubview(phone)
-    }
-    
-    func createBlurBackground(size: CGSize) {
-        
-        let bckgrndImgViw = UIImageView()
-        bckgrndImgViw.contentMode = .ScaleAspectFill
-        bckgrndImgViw.clipsToBounds = true
-        bckgrndImgViw.frame = CGRectMake(
-            0,
-            0,
-            UIScreen.mainScreen().bounds.size.width,
-            UIScreen.mainScreen().bounds.size.width
-        )
-        dispatch_async(dispatch_get_main_queue()) { () -> Void in
-           
-            bckgrndImgViw.image = blurredImageWithImage(
-                ImageUtil.cropToSquare(
-                    image: self.image, contextSize: CGSize(
-                        width: UIScreen.mainScreen().bounds.width,
-                        height: UIScreen.mainScreen().bounds.width
-                    )
-                )
-                
-            )
-//            dispatch_sync(dispatch_get_main_queue(), { () -> Void in
-                self.mainImageView.insertSubview(bckgrndImgViw, atIndex: 0)
+//    //MARK! - Imaage createion
+//    func createPhoneSkinImage(phoneInfo: PhoneSkin) {
+//        let phone = UIImageView()
+//        
+//        NSLog("mainImageView.bounds.size = \(mainImageView.bounds.size)")
+//        NSLog("mainImageView.frame.size = \(mainImageView.frame.size)")
+//
+//        
+//        phone.image = phoneInfo.handImage
+//        phone.contentMode = .ScaleAspectFill
+//        phone.userInteractionEnabled = true
+//        phone.frame = CGRectMake(
+//            0,
+//            0,
+//            UIScreen.mainScreen().bounds.size.width,  // should not be here
+//            UIScreen.mainScreen().bounds.size.width   // should not be here
+//        )
+//        phone.addSubview(createOnScreenScrollView(phone, phoneInfo: phoneInfo))
+//        mainImageView.addSubview(phone)
+//    }
+//    
+//    func createBlurBackground(size: CGSize) {
+//        
+//        let bckgrndImgViw = UIImageView()
+//        bckgrndImgViw.contentMode = .ScaleAspectFill
+//        bckgrndImgViw.clipsToBounds = true
+//        bckgrndImgViw.frame = CGRectMake(
+//            0,
+//            0,
+//            UIScreen.mainScreen().bounds.size.width,
+//            UIScreen.mainScreen().bounds.size.width
+//        )
+//        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)) { () in
+//           
+//            let bluredImage = blurredImageWithImage(
+//                ImageUtil.cropToSquare(
+//                    image: self.image, contextSize: CGSize(
+//                        width: UIScreen.mainScreen().bounds.width,
+//                        height: UIScreen.mainScreen().bounds.width
+//                    )
+//                )
+//            )
+//            dispatch_async(dispatch_get_main_queue(), {
+//                bckgrndImgViw.image = bluredImage
 //            })
-        }
-
-        
-    }
-    
-    
-    
-    //TODO! change THIS!
-    var onScreenImgViw: UIImageView!
-    var scrollView: UIScrollView!
-    func createOnScreenScrollView(parentView: UIView, phoneInfo: PhoneSkin) -> UIScrollView{
-        onScreenImgViw = UIImageView(image: image)
-        onScreenImgViw.sizeToFit()
-        print("---------phoneInfo.startPoint = \(phoneInfo.startPoint)")
-        scrollView = UIScrollView(frame: CGRectMake(
-            phoneInfo.startPoint.x + parentView.frame.origin.x,
-            phoneInfo.startPoint.y + parentView.frame.origin.y,
-            phoneInfo.screenSize.width,
-            phoneInfo.screenSize.height
-            )
-        )
-        scrollView.delegate = self
-        
-        scrollView.backgroundColor = UIColor.blackColor()
-        scrollView.contentSize = onScreenImgViw.frame.size
-        scrollView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight ]
-        
-        scrollView.addSubview(onScreenImgViw)
-        return scrollView
-        
-    }
-    override func viewWillAppear(animated: Bool) {
-        
-        //properties of window
-        self.navigationController?.setNavigationBarHidden(false, animated: true)
-        super.viewWillAppear(animated)
-    }
-    
-    //MARK! - scrollView Zoom
-    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
-        return onScreenImgViw
-    }
-    override func viewWillLayoutSubviews() {
-        setZoomScale()
-    }
-    func setZoomScale() {
-        let imageViewSize = onScreenImgViw.bounds.size
-        let scrollViewSize = scrollView.bounds.size
-        let widthScale = scrollViewSize.width / imageViewSize.width
-        let heightScale = scrollViewSize.height / imageViewSize.height
-        
-        scrollView.minimumZoomScale = min(widthScale, heightScale)
-        scrollView.zoomScale = heightScale
-        print( imageViewSize, scrollViewSize, imageViewSize.width / 2 - scrollViewSize.width / 2)
-        print(scrollView.contentSize)
-        scrollView.contentOffset = CGPoint(
-            x: scrollView.contentSize.width / 2 - scrollViewSize.width / 2,
-            y: 0.0)
-        
-    }
-    func scrollViewDidZoom(scrollView: UIScrollView) {
-        let imageViewSize = onScreenImgViw.frame.size
-        let scrollViewSize = scrollView.bounds.size
-        
-        let verticalPadding = imageViewSize.height < scrollViewSize.height ? (scrollViewSize.height - imageViewSize.height) / 2 : 0
-        let horizontalPadding = imageViewSize.width < scrollViewSize.width ? (scrollViewSize.width - imageViewSize.width) / 2 : 0
-        
-        scrollView.contentInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding, bottom: verticalPadding, right: horizontalPadding)
-        
-    }
+//        }
+//        self.mainImageView.insertSubview(bckgrndImgViw, atIndex: 0)
+//        
+//    }
+//    
+//    
+//    
+//    
+//    //TODO! change THIS!
+//    var onScreenImgViw: UIImageView!
+//    var scrollView: UIScrollView!
+//    
+//    func createOnScreenScrollView(parentView: UIView, phoneInfo: PhoneSkin) -> UIScrollView{
+//        onScreenImgViw = UIImageView(image: image)
+//        onScreenImgViw.sizeToFit()
+//        print("---------phoneInfo.startPoint = \(phoneInfo.startPoint)")
+//        scrollView = UIScrollView(frame: CGRectMake(
+//            phoneInfo.startPoint.x + parentView.frame.origin.x,
+//            phoneInfo.startPoint.y + parentView.frame.origin.y,
+//            phoneInfo.screenSize.width,
+//            phoneInfo.screenSize.height
+//            )
+//        )
+//        scrollView.delegate = self
+//        
+//        scrollView.backgroundColor = UIColor.blackColor()
+//        scrollView.contentSize = onScreenImgViw.frame.size
+//        scrollView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight ]
+//        
+//        scrollView.addSubview(onScreenImgViw)
+//        return scrollView
+//        
+//    }
+//    override func viewWillAppear(animated: Bool) {
+//        
+//        //properties of window
+//        self.navigationController?.setNavigationBarHidden(false, animated: true)
+//        super.viewWillAppear(animated)
+//    }
+//    
+//    //MARK! - scrollView Zoom
+//    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+//        return onScreenImgViw
+//    }
+//    override func viewWillLayoutSubviews() {
+//        setZoomScale()
+//    }
+//    func setZoomScale() {
+//        let imageViewSize = onScreenImgViw.bounds.size
+//        let scrollViewSize = scrollView.bounds.size
+//        let widthScale = scrollViewSize.width / imageViewSize.width
+//        let heightScale = scrollViewSize.height / imageViewSize.height
+//        
+//        scrollView.minimumZoomScale = min(widthScale, heightScale)
+//        scrollView.zoomScale = heightScale
+//        print( imageViewSize, scrollViewSize, imageViewSize.width / 2 - scrollViewSize.width / 2)
+//        print(scrollView.contentSize)
+//        scrollView.contentOffset = CGPoint(
+//            x: scrollView.contentSize.width / 2 - scrollViewSize.width / 2,
+//            y: 0.0)
+//        
+//    }
+//    func scrollViewDidZoom(scrollView: UIScrollView) {
+//        let imageViewSize = onScreenImgViw.frame.size
+//        let scrollViewSize = scrollView.bounds.size
+//        
+//        let verticalPadding = imageViewSize.height < scrollViewSize.height ? (scrollViewSize.height - imageViewSize.height) / 2 : 0
+//        let horizontalPadding = imageViewSize.width < scrollViewSize.width ? (scrollViewSize.width - imageViewSize.width) / 2 : 0
+//        
+//        scrollView.contentInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding, bottom: verticalPadding, right: horizontalPadding)
+//        
+//    }
     
     
     func createFiltersScrollMenu() {
